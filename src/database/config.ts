f84d638bfc1bf6d120dd99af8afa32f86f97ca0e
wolwer <ricardo@dtmm.com.br>
2023-03-30 23:40:03 -0300

    "Acerto para rodar localmente"

--- src/database/config.ts
+++ src/database/config.ts
@@ -4,20 +4,20 @@ import { Comment } from '../entities/comment.entity';
 
 export const connection = createConnection({
     type: 'postgres',
-    host: 'ec2-3-230-122-20.compute-1.amazonaws.com',
+    host: 'localhost',
     port: 5432,
-    username: 'einodddhwgywnr',
-    password: 'b3f22b576b2b28403f520d8f267f9def18c1e3b2b49566e1ac7a0bbe0eaea352',
-    database: 'dn2qpo3t1vpov',
+    username: 'postgres',
+    password: '123',
+    database: 'commentNode',
     entities: [
         Comment
     ],
-    // logging: true,
-    // synchronize: true,
-    ssl: true,
-    extra: {
-        ssl: {
-            rejectUnauthorized: false,
-        }
-    }
+    logging: true,
+    synchronize: true,
+    // ssl: false,
+    // extra: {
+    //     ssl: {
+    //         rejectUnauthorized: false,
+    //     }
+    // }
 });
